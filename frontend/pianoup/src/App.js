23547577b4321c804be8d175866f43eb45d8f55e
sebastian-olivaresc <sebastian.olivaresc@sansano.usm.cl>
2023-05-17 16:35:22 -0400

    se agrego menu principal

--- frontend/pianoup/src/App.js
+++ frontend/pianoup/src/App.js
@@ -1,24 +1,47 @@
 import './App.css';
 
-
+import { Routes, Route, BrowserRouter, Link } from 'react-router-dom';
+import Image from 'react-bootstrap/Image';
 
 import Entrypoint from './Entrypoint';
 import Login from './user/Login';
 import Register from './user/Register';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-
-
+import Etapa1 from './etapas/Etapa1';
+import Etapa2 from './etapas/Etapa2';
+import Etapa3 from './etapas/Etapa3';
 
 function App() {
 
   return (
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Entrypoint />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-           </Routes>
-            <h1>This is a test</h1>
+            <BrowserRouter>
+                <Routes>
+                  <Route path="/" element={<Entrypoint />} />
+                  <Route path="/login" element={<Login/>} />
+                  <Route path="/register" element={<Register/>} />
+                  <Route path="/etapa1" element={<Etapa1 />} />
+                  <Route path="/etapa2" element={<Etapa2 />} />
+                  <Route path="/etapa3" element={<Etapa3 />} />
+                </Routes>
+                <div className='d-flex justify-content-center'>
+                    <Link to="/etapa1">
+                        <Image className="m-5" src="images/act1.png"
+                            rounded
+                            width={300} 
+                            height={300}/>
+                    </Link>
+                    <Link to="/etapa2">
+                        <Image className="m-5" src="images/act2.png"
+                            rounded
+                            width={300} 
+                            height={300}/>
+                    </Link>
+                    <Link to="/etapa3">
+                        <Image className="m-5" src="images/act3.png"
+                            rounded
+                            width={300} 
+                            height={300}/>
+                    </Link>
+                </div>
           </BrowserRouter>
   );
 }
---
+++ frontend/pianoup/src/etapas/Etapa3.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Etapa1() {
+  return (
+    <div>
+      <h1>Etapa 1</h1>
+      {/* Add your game content here */}
+    </div>
+  );
+}
+
+export default Etapa1;
