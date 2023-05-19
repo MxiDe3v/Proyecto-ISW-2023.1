94a4d4284af9e8122b93f42ef4fb9c192ac5a34b
to+ <tomas.carmona@usm.cl>
2023-05-18 22:11:09 -0400

    Primera versión de página. Notas tocables. Falta arrastrar

--- frontend/pianoup/src/App.js
+++ frontend/pianoup/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Entrypoint from './Entrypoint';
 import Login from './user/Login';
 import Register from './user/Register';
+import Notas from './etapas/etapa-1/notas-musicales/Notas'
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 
@@ -17,6 +18,7 @@ function App() {
               <Route path="/" element={<Entrypoint />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
+              <Route path="/etapas/1/notas_musicales" element={<Notas/>}/>  {/* No se puede llegar desde página. Usar ruta en navegador. */}
            </Routes>
           </BrowserRouter>
   );
