6a746e8ac7c56b4234567041c18c80eb9a3d52b2
José Miguel Rojas González <jose.rojas@alumnos.inf.utfsm.cl>
2023-05-20 10:32:43 -0400

    Update Metronome.js

--- frontend/pianoup/src/components/Metronome.js
+++ frontend/pianoup/src/components/Metronome.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom"
 import Button from '@mui/material/Button';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import StopIcon from '@mui/icons-material/Stop';
@@ -85,6 +86,9 @@ class Metronome extends Component {
 
     return (
         <>
+          <Link to="/etapa1"> 
+                <button>Volver</button>
+            </Link>
           <div className="metronome">
             <div className="bpm-slider">
               <h2 className="title-slider">Corazon a Pulso</h2>
