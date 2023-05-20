b291eaf0fbe128ca418daa3230d6fbc05300da30
José Miguel Rojas González <jose.rojas@alumnos.inf.utfsm.cl>
2023-05-20 09:18:39 -0400

    Update Etapa1.js

--- frontend/pianoup/src/etapas/Etapa1.js
+++ frontend/pianoup/src/etapas/Etapa1.js
@@ -9,7 +9,25 @@ function Etapa1() {
 		<>
 			<h1>Etapa 1</h1>
 			<div className='d-flex justify-content-center'>
-
+				<Card className='m-5'>
+					<Link to='/llavesol'>
+						<CardMedia 
+							component='img'
+							height='300'
+							image='images/act1.png'
+						/>
+					</Link>
+					<CardContent>
+						<Link to='/llavesol'>
+							<Typography gutterBottom variant='h3'>
+                                Llave de Sol
+							</Typography>
+						</Link>
+						<Typography variant='body2'>
+							Progreso : {}
+						</Typography>
+					</CardContent>
+				</Card>
 				<Card className='m-5'>
 					<Link to='/notas'>
 						<CardMedia 
