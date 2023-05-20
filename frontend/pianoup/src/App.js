312f12c6fced7c5b1c7b166e4acdc17475364af6
sebastian.olivares <sebastian.olivaresc@sansano.usm.cl>
2023-05-20 00:07:54 -0400

    modified:   App.js

--- frontend/pianoup/src/App.js
+++ frontend/pianoup/src/App.js
@@ -1,28 +1,22 @@
 import './App.css';
 
-<<<<<<< HEAD
 import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-=======
+
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
 import Image from 'react-bootstrap/Image';
->>>>>>> f70b861e0b8e6a7c816bb74a7f055abb88d270cf
 
 import Home from './Home';
 import Login from './user/Login';
 import Register from './user/Register';
-<<<<<<< HEAD
 
 import Notas from './etapas/etapa-1/notas-musicales/Notas'
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 import Notas from './etapas/etapa-1/notas-musicales/Notas'
 
-=======
 
 import Menu from './Menu';
->>>>>>> f70b861e0b8e6a7c816bb74a7f055abb88d270cf
 import Etapa1 from './etapas/Etapa1';
 import Etapa2 from './etapas/Etapa2';
 import Etapa3 from './etapas/Etapa3';
@@ -30,7 +24,6 @@ import { EmparejarDuracion } from './components/EmparejarDuracion';
 import { EmparejarTimbre } from './components/EmparejarTimbre';
 import { EmparejarIntensidad } from './components/EmparejarIntensidad';
 import { EmparejarFrecuencia } from './components/EmparejarFrecuencia';
-<<<<<<< HEAD
 
 import Memorice from './Memorice';
 import Tempo from './Tempo';
@@ -38,8 +31,6 @@ import Tempo from './Tempo';
 import Card from './components/Card';
 
 import { images } from './import';
-=======
->>>>>>> f70b861e0b8e6a7c816bb74a7f055abb88d270cf
 
 function App() {
 
@@ -106,7 +97,6 @@ function App() {
   return (
         <div>
             <Routes>
-<<<<<<< HEAD
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login/>} />
                 <Route path="/register" element={<Register/>} />
@@ -136,17 +126,6 @@ function App() {
       </div>
     </div>
     */
-=======
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login/>} />
-              <Route path="/register" element={<Register/>} />
-              <Route path="/etapa1" element={<Etapa1 />} />
-              <Route path="/etapa2" element={<Etapa2 />} />
-              <Route path="/etapa3" element={<Etapa3 />} />
-            </Routes>
-            <EmparejarDuracion/>
-      </div>
->>>>>>> f70b861e0b8e6a7c816bb74a7f055abb88d270cf
   );
 }
 
