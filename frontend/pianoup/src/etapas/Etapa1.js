ccd8cb5dab6a7ee966bfce76ec2dcd08e947a306
Sebastián Ignacio Olivares Carrasco <sebastian.olivares@alumnos.inf.utfsm.cl>
2023-05-19 23:40:37 -0400

    Agrega grupo-A a test

---
+++ frontend/pianoup/src/etapas/Etapa1.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import { Card, CardContent, Typography, CardMedia } from "@mui/material";
+import ResponsiveAppBar from '../ResponsiveAppBar';
+import './Etapa.css';
+
+function Etapa1() {
+	return (
+		<>
+			<h1>Etapa 1</h1>
+
+			<div className='d-flex justify-content-center'>
+
+				<Card className='m-5'>
+					<Link to='/memorice'>
+						<CardMedia 
+							component='img'
+							height='300'
+							image='images/act1.png'
+						/>
+					</Link>
+					<CardContent>
+						<Link to='/memorice'>
+							<Typography gutterBottom variant='h3'>
+								Memorice
+							</Typography>
+						</Link>
+						<Typography variant='body2'>
+							Progreso : {}
+						</Typography>
+					</CardContent>
+				</Card>
+				
+				<Card className='m-5'>
+					<Link to='/tempo'>
+						<CardMedia 
+							component='img'
+							height='300'
+							image='images/act1.png'
+						/>
+					</Link>
+					<CardContent>
+						<Link to='/tempo'>
+							<Typography gutterBottom variant='h3'>
+							    Tempo	
+							</Typography>
+						</Link>
+						<Typography variant='body2'>
+							Progreso : {}
+						</Typography>
+					</CardContent>
+				</Card>
+
+			</div>
+		</>
+	);
+}
+
+export default Etapa1;
---
+++ frontend/pianoup/src/etapas/Etapa3.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import { Card, CardContent, Typography, CardMedia } from "@mui/material";
+import ResponsiveAppBar from '../ResponsiveAppBar';
+import './Etapa.css';
+
+function Etapa3() {
+	return (
+		<>
+			<h1>Etapa 3</h1>
+
+			<div className='d-flex justify-content-center'>
+
+				<Card className='m-5'>
+					<Link to='/pulso'>
+						<CardMedia 
+							component='img'
+							height='300'
+							image='images/act3.png'
+						/>
+					</Link>
+					<CardContent>
+						<Link to='/pulso'>
+							<Typography gutterBottom variant='h3'>
+								Memorice
+							</Typography>
+						</Link>
+						<Typography variant='body2'>
+							Progreso : {}
+						</Typography>
+					</CardContent>
+				</Card>
+
+				<Card className='m-5'>
+					<Link to='/pulso'>
+						<CardMedia 
+							component='img'
+							height='300'
+							image='images/act3.png'
+						/>
+					</Link>
+					<CardContent>
+						<Link to='/pulso'>
+							<Typography gutterBottom variant='h3'>
+								Compositor
+							</Typography>
+						</Link>
+						<Typography variant='body2'>
+							Progreso : {}
+						</Typography>
+					</CardContent>
+				</Card>
+			
+			</div>
+		</>
+	);
+}
+
+export default Etapa3;
